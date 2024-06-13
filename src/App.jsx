7a9405413e6dc8cbc7bf9a1881a185818d8ce414
aysel <elekberova2003@bk.ru>
2024-06-14 02:29:48 +0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Nav from "./components/nav"
+
+function App() {
+
+
+  return (
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+      <Nav/>
+    </h1>
+  )
+}
+
+export default App
